b990966f79f7381bb48a394ce53ebae16595e515
RiteshVaish <ritesh_vaish@yahoo.com>
2019-05-17 00:58:34 -0700

    updated correct files

--- Instructions/scrape_mars.py
+++ Instructions/scrape_mars.py
@@ -28,7 +28,7 @@ def scrape():
     news_p= news_soup.find('div', class_='article_teaser_body').text
     mars_data["news_title"] = news_title
     mars_data["news_p"] = news_p
-    
+    print(news_p)
     # 2. Scrapping Mars Space Images - Featured Image
     #print("into scrape function 2................")
     base_url = "https:/www.jpl.nasa.gov/"
@@ -52,7 +52,8 @@ def scrape():
     mars_weather_temp = soup.find('div', class_='js-tweet-text-container').find("a").text
     mars_weather = soup.find('div', class_='js-tweet-text-container').text.replace(mars_weather_temp,"")
     mars_data['mars_weather']=mars_weather
-    
+    print("===============Mars Weather===========")
+    print(mars_weather)
     ## print(mars_data['mars_weather'])
     
     # 4. Scrape Mars Facts
@@ -79,9 +80,18 @@ def scrape():
     hemisphere_image_urls = []
 
     for i in range(len(result_set_url)):
-        img_url = base_url+result_set_url[i]['href']
+        target_url_1 = result_set_url[i]
+        target_url_2 = target_url_1['href']
+        target_url_full = base_url+target_url_2
+        #print(target_url_full)
+        response = requests.get(target_url_full)
+        soup = bs(response.text, 'lxml')
+        result_set_url_4 = soup.find('img', class_='wide-image')['src']
+        result_set_url_5=base_url+result_set_url_4
         title=result_set_titles[i].text
-        hemisphere_image_urls.append({'title' : title, 'img_url' : img_url})
+        print(title)
+        print(result_set_url_5)
+        hemisphere_image_urls.append({'title' : title, 'img_url' : result_set_url_5})
 
     mars_data['hemisphere_image_urls']=hemisphere_image_urls
     ##print(mars_data['hemisphere_image_urls'])
